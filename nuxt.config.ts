c6edade072c990d8d1f09bbda2296e787cc7d0b9
Amiin Najjar <amiin.najjar@stud-mail.uni-wuerzburg.de>
2023-10-03 15:23:08 +0200

    website push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app:{
-    baseURL: '/',
-    buildAssetsDir: 'assets'
-  },
   devtools: { enabled: true }
 })
