06c7ed1b1f440be84180c84b4fa7b4e953e84c95
Amiin Najjar <amiin.najjar@stud-mail.uni-wuerzburg.de>
2023-10-03 15:47:39 +0200

    besipiel

--- app.vue
+++ app.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
  let message = ref<number >(1)
-
+  let bobo= ref< number> (665);
 </script>
 
 
 <template>
   <div>
     <button @click="message++">Click me</button>
+    <button @click="bobo"> Yooo</button>
     <h1>{{message}}</h1>
   </div>
 </template>
