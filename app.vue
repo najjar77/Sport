6472684f65f3df1b108056831620366411137000
madisoufian <soufian.madi@andrena.de>
2023-10-03 15:11:05 +0200

    Add infrastructure for Github

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+ let message = ref<number >(1)
+
+</script>
+
+
 <template>
   <div>
-    <h1>Hello MotherFucker</h1>
+    <button @click="message++">Click me</button>
+    <h1>{{message}}</h1>
   </div>
 </template>
+
+
+<style>
+.bobo{
+  background: aqua;
+}
+
+
+</style>
