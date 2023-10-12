3d8a33f360a51aa84bf5f6514b0c84353b85ed09
madisofian <madimadisofian@gmail.com>
2023-10-12 18:48:07 +0200

    Set dist as build folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app:{
+    buildAssetsDir: 'dist',
     baseURL: '/Sport/'
   },
   devtools: { enabled: true }
