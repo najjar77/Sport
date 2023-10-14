f79bcdad9f498a4834920d21673dce1521770677
madisofian <madimadisofian@gmail.com>
2023-10-14 15:48:23 +0200

    Fix asset path imoport issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app:{
-    baseURL: '/Sport/'
+    baseURL: '/Sport/',
+    buildAssetsDir: 'assets',
   },
   devtools: { enabled: true }
 })
