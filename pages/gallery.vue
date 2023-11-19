39f7a18e9a5bc46bd4c0c0c06235e14cdb3eabc4
Amiin Najjar <amiin.najjar@stud-mail.uni-wuerzburg.de>
2023-11-19 15:28:16 +0100

    Add visibility Button

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
-
 import Form from "~/Components/Form.vue";
 import Table from "~/Components/Table.vue";
+
+const isFormVisible = ref(false);
+function openForm() {
+  isFormVisible.value = !isFormVisible.value;
+}
+
+
 </script>
 
 <template>
-  <Form/>
-  <v-divider thickness="5"/>
-  <Table/>
+  <Form v-if="isFormVisible"/>
+
+  <v-btn prepend-icon="mdi-plus-circle" block rounded="xl" @click="openForm"> Add Entry</v-btn>
+  <v-divider thickness="15"/>
+  <Table v-if="!isFormVisible"/>
 
 </template>
 
