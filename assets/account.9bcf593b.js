a9e8236df6091ee526e3773f1e1eca93a9c5da0f
amiin <amiin.najjar@metafinanz.de>
2023-12-19 23:33:00 +0100

    Updates

--- assets/account.9bcf593b.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./entry.ff2c5f69.js";const n={};function r(t,e){return null}const o=c(n,[["render",r]]);export{o as default};
--- assets/home.378bf459.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./entry.ff2c5f69.js";const r={};function c(n,t){return null}const o=e(r,[["render",c]]);export{o as default};
